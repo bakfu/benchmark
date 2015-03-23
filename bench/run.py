9e04889c83bac165f69cb215f44acc4a5280d6e4
prmths128 <plloret@gmx.com>
2015-03-23 19:46:43 +0100

    Added files.

---
+++ bench/run.py
@@ -0,0 +1,169 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+import os
+
+import itertools
+from itertools import product
+from itertools import izip_longest  
+
+
+import yaml
+import json
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+log = logging.getLogger('bench')
+
+import bakfu
+import tools
+
+
+# Print results out to a file
+# TODO: send results to a DB
+result_logger = logging.getLogger('bench_results')
+logfile = logging.FileHandler('results.log')
+formatter = logging.Formatter('%(message)s')
+logfile.setFormatter(formatter)
+result_logger.addHandler(logfile ) 
+result_logger.setLevel(logging.INFO)
+
+
+
+def filterFor( path, element ):
+    print(path,element)
+
+def walkDict(vDict, func, caller, context, path=()):
+    '''
+    Process a dictionnary recursively.
+    
+    Looks for string values starting with special characters and process them accordingly : 
+      % : replace variable
+          ex : %name will be replaced by the name variable defined in the benchmark parameters
+      !: evaluates the expression
+          ex : !range(1,4)
+    '''
+    for k,v in vDict.items():
+        if type(vDict[k]) == dict:
+            walkDict( vDict[k], func, caller, context, path+(k,) )
+        elif isinstance(v,str):
+            if len(v)>0 and v[0]=='%':
+                #func(vDict,k,v, caller, context)
+                vDict[k] =  context[v[1:]]
+            if len(v)>0 and v[0]=='!':
+                #func(vDict,k,v, caller, context)
+                vDict[k] =  eval(v[1:])
+        elif isinstance(v,list):
+            for elt in v:
+                if type(elt) == dict:
+                    walkDict( elt, func, caller, context, path+("/list/",) )                
+        else:
+            pass
+            
+def replacer(vDict,k,v,caller,context):
+    vDict[k] =  context[v[1:]]
+
+
+
+class Iterator(object):
+    '''
+    Returns  an iterator over a list of values.
+    '''
+    def __init__(self, elements):
+        log.debug('Creating iterator {}'.format(elements))
+
+
+class Parameter(object):
+    '''Returns and register an object storing a value/iterator for a given parameter.'''    
+    def __init__(self, name, value):
+        self.name = name
+        self.values = eval(value[1:])
+
+
+class BenchElement(object):
+    ''' 
+    A run for given set of parameters.
+    '''
+    def __init__(self, name, bench_data, parameters):
+        self.name = name
+        self.bench_data = bench_data
+        self.parameters = parameters
+
+    def run(self):
+        log.info('Running : {name}'.format(name=self.name))
+        log.info('  parameters: {}'.format(self.parameters))
+        
+
+        
+        baf = bakfu.Chain.load_chain(self.bench_data)
+        import pdb;pdb.set_trace()
+        score = baf.get_chain('score')
+        #print("--->score",score)
+        results = dict(
+            name=self.name,
+            bench_data=self.bench_data,
+            parameters=str(self.parameters),
+            #parameters contain lambdas -> must be serialized as str
+            score=score,
+            )
+
+        return results
+
+class Benchmark(object):
+    def __init__(self, path):
+        file = open(path)
+        self.bench_data = yaml.load(file.read())
+        self.parameters = []
+        self.bench_results = []
+        parameters_data = self.bench_data['bench']['parameters'].items()
+        
+        result_logger.info('Starting benchmark ')
+        result_logger.info('name :  {}'.format(self.bench_data['bench']['name']))
+
+
+        for parameter_name, parameter_value in parameters_data:
+  
+            new_parameter = Parameter(parameter_name, parameter_value)
+            self.parameters.append(new_parameter)
+
+    def run(self):
+        parameter_combinations = []
+        dictList = {
+                parameter.name:parameter.values 
+                for parameter in self.parameters}
+
+        parameter_combinations = [
+            dict(izip_longest(dictList, v)) 
+            for v in product(*dictList.values())]
+
+        for idx, parameter_list in enumerate(parameter_combinations):
+            result_logger.info('\n\n')
+            result_logger.info("---------------------------")
+            result_logger.info("Run : ")
+            result_logger.info("---------------------------")            
+            result_logger.info('Parameter list :')
+            result_logger.info(parameter_list)
+            data = self.bench_data.copy()
+            data.pop('bench')
+            walkDict(data, replacer, self, parameter_list)
+
+            bench_element = BenchElement(idx, data, parameter_list)
+            result = bench_element.run()
+            self.bench_results.append(result)
+            
+        #write results to json file
+        logger.info("Writing results to results.json file.")
+        with open("results.json","w") as f:
+            f.write(json.dumps(self.bench_results))
+            
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('''Usage : run.py bench.yaml''')
+    else:    
+        path = sys.argv[1]
+        bench = Benchmark(path)
+        bench.run()
---
+++ bench/tools.py
@@ -0,0 +1,93 @@
+
+import random
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+import nltk        
+
+
+from bakfu.core.routes import register
+from bakfu.core.classes import Processor
+
+
+import logging
+logger = logging.getLogger('bench')
+
+result_logger = logging.getLogger('bench_results')
+
+
+logger = logging.getLogger('bench')
+
+@register('bench.ml1')
+class BenchProcess(Processor):   
+    '''
+    Run a random forest and test quality.    
+    '''
+    def run(self, baf, *args, **kwargs):
+        print('BENCH....')
+        baf = self.chain = bakfu.Chain(lang=lang)
+
+    def __init__(self, *args, **kwargs):
+        super(BenchProcess, self).__init__(*args, **kwargs)
+
+    def run(self, caller, *args, **kwargs):
+        super(BenchProcess, self).run(caller, *args, **kwargs)
+        baf = caller
+        data_source = caller.get_chain('data_source')
+
+        language = baf.get('language')
+
+        stop_words = nltk.corpus.stopwords.words(baf.get('language'))
+        if language == 'french':
+            stop_words.extend([u'les',u'la|le',u'\xeatre',u'@card@',u'avoir',u'@ord@',u'aucun',u'oui',u'non',
+                         u'aucune',u'jamais',u'voir',u'n/a',u'ras',u'nil',u'nous',
+                         u'chez','nous','quand',u'',u'',u'Les',u'[i]',u'si',u'\xe0','('])
+
+        data_source.get_data()
+
+
+        labels  = baf.get_chain('targets')
+        answers = data_source.get_data()
+
+
+        classifier = RandomForestClassifier(n_estimators=50)
+        X=baf.data['vectorizer_result']
+        
+        score=[0,0]        
+        NUM_RUNS = 50
+        SAMPLE_SIZE = 50
+
+        if os.environ('BENCH_FAST','0')=='1':
+            #Fast mode...
+            NUM_RUNS = 5
+            SAMPLE_SIZE = 2
+
+        if len(answers)<SAMPLE_SIZE:
+            SAMPLE_SIZE = len(answers)
+        
+        
+        for run in range(NUM_RUNS):
+            print("run {}".format(run))
+            for i in range(SAMPLE_SIZE):
+            #for i in range(X.shape[0]):
+                print(i)
+                #X2 = np.array([X[i].toarray()[0] for j in range(X.shape[0])] if j!=i)
+                #labels2 = [labels[j] for j in range(X.shape[0])] if j!=i]
+                X2 = np.array([X[j].toarray()[0] for j in range(X.shape[0]) if i!=j])
+                labels2 = [labels[j] for j in range(X.shape[0]) if j!=i]
+
+                classifier.fit(X2,np.array(labels2))
+                pred = classifier.predict([X.toarray()[i],])[0]
+
+                if pred==labels[i]:
+                    score[0]+=1
+                else:
+                    score[1]+=1
+
+        result_logger.info(score)
+        R=score[0]/float(sum(score))*100
+        result_logger.info("Score :   good : \t {} ; \t bad : \t : {}  \t  ratio {}%" .format(score[0],score[1],R))
+
+        self._data['score'] = (score[0],score[1],R)
+
+        
+        return self
